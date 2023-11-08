4cb56c0915ee9ca71f00f073cd5011e813a76f13
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-11-08 11:29:47 -0300

    feat[HandleResponse]: criando uma classe para gerenciar a resposta ao client

---
+++ backend/src/utils/handle-response.ts
@@ -0,0 +1,18 @@
+import User from "@entities/user";
+import { Response } from "express";
+
+export default class HandleResponse {
+    constructor(
+        public statusCode: number,
+        public message?: string | undefined,
+        public data?: Partial<User> | Partial<User>[]
+    ) { }
+
+    execute(res: Response) {
+        return res.status(this.statusCode).json({
+            statusCode: this.statusCode,
+            message: this.message,
+            user: this?.data
+        })
+    }
+}
