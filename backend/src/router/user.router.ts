594e2ab6fea9ea64d49b5c18aee4b905b170369b
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-11-10 13:21:50 -0300

    refactor: Refatorando o router do usuário. Adicionei uma interface para gerenciar o request da requisição

--- backend/src/router/user.router.ts
+++ backend/src/router/user.router.ts
@@ -2,7 +2,8 @@ import UserController from "@controllers/user.controller";
 import authMiddleware from "@middlewares/auth.midd";
 import PrismaUserRepo from "@repositories/user/prisma-user.repo";
 import UserService from "@services/user.service";
-import { Request, Response, Router } from "express";
+import HandleRequest from "@utils/handle-request";
+import { Router } from "express";
 
 const userRouter = Router()
 
@@ -10,11 +11,11 @@ const repo = new PrismaUserRepo()
 const service = new UserService(repo)
 const controller = new UserController(service)
 
-userRouter.post('/', (req:Request,res:Response) => (controller.handleCreateUser({req,res})))
+userRouter.post('/', ({ req, res }: HandleRequest) => (controller.handleCreateUser({ req, res })))
 userRouter.use(authMiddleware)
-userRouter.get('/', (req:Request,res:Response) => (controller.handleGetAllUsers({req,res})))
-userRouter.get('/profile', (req:Request,res:Response) => (controller.handleGetUserById({req,res})))
-userRouter.delete('/:id', (req:Request,res:Response) => (controller.handleDeleteUser({req,res})))
-userRouter.patch('/:id', (req:Request,res:Response) => (controller.handleUpdateUser({req,res})))
+userRouter.get('/', ({ req, res }: HandleRequest) => (controller.handleGetAllUsers({ req, res })))
+userRouter.get('/profile', ({ req, res }: HandleRequest) => (controller.handleGetUserById({ req, res })))
+userRouter.delete('/:id', ({ req, res }: HandleRequest) => (controller.handleDeleteUser({ req, res })))
+userRouter.patch('/:id', ({ req, res }: HandleRequest) => (controller.handleUpdateUser({ req, res })))
 
 export default userRouter
