97a9377551509f00ca7de18a413ea65edccb5502
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-11-10 16:52:17 -0300

    feat: adicionando updateCategory no service

--- backend/src/services/category.service.ts
+++ backend/src/services/category.service.ts
@@ -1,5 +1,6 @@
 import CreateCategoryDto from "@entities/category/dto/create-category.dto";
 import NotFoundError from "@errors/not-found.error";
+import { Category } from "@prisma/client";
 import PrismaCategoryRepo from "@repositories/category/prisma-category.repo";
 
 export default class CategoryService{
@@ -13,5 +14,11 @@ export default class CategoryService{
         const categories = await this.prismaCategoryRepo.getAll()
         if(categories.length === 0)throw new NotFoundError('Nenhuma categoria encontrada')
         return categories
+    }
+
+    async updateCategory({category_id,name}:Category){
+        const category = await this.prismaCategoryRepo.getById({category_id})
+        if(!category)throw new NotFoundError('Categoria não encontrada')
+        return category
     }
 }
