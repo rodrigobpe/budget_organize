c6482274b524b501a3aa1df3ae2b1bf35ed854bc
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-11-12 11:58:40 -0300

    feat (creditCardRouter): criando endpoint para creditCard

---
+++ backend/src/router/credit-card.router.ts
@@ -0,0 +1,17 @@
+import CreditCardController from "@controllers/credit-card.controller";
+import authMiddleware from "@middlewares/auth.midd";
+import PrismaCreditCardRepo from "@repositories/credit-card/prisma-credit-card.repo";
+import PrismaUserRepo from "@repositories/user/prisma-user.repo";
+import CreditCardService from "@services/credit-card.service";
+import { Request, Response, Router } from "express";
+
+const creditCardRouter = Router()
+
+const userRepo = new PrismaUserRepo()
+const creditCardRepo = new PrismaCreditCardRepo()
+const service = new CreditCardService(userRepo, creditCardRepo)
+const controller = new CreditCardController(service)
+
+creditCardRouter.use(authMiddleware)
+creditCardRouter.post('/users/:user_id/credit-cards', (req: Request, res: Response) => controller.handleCreateCreditCard({ req, res }))
+creditCardRouter.get('/credit-cards', (req: Request, res: Response) => controller.handleGetAllCreditCards({ req, res }))
